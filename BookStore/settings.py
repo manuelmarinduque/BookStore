8f03b133022929747f5f2f7694a187f8ec3bd9ee
Manuel Marin <manuel.duque5.2@gmail.com>
2021-08-22 21:56:37 -0500

    Se añade la aplicción libro a las apps instaladas en Django.

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'libro.apps.LibroConfig'
+    'libro',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
