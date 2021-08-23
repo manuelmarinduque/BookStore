690e8629f97712c3ddd6e6f7399495b18b8295e1
Manuel Marin <manuel.duque5.2@gmail.com>
2021-08-23 12:37:41 -0500

    Se crea la url de la app libro, empleando la CBV que brinda graphene_django.

--- libro/urls.py
+++ libro/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from graphene_django.views import GraphQLView
 
 # Create your urls here.
+
 
 app_name = 'libro'
 
 urlpatterns = [
-
+    path('', GraphQLView.as_view(graphiql=True), name='index')
 ]
